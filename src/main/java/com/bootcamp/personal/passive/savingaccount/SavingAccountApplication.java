e5dcdc2635ed1a6d7e74340fdec248be42684340
samuel14luis <luismendoza.samuel@gmail.com>
2022-04-15 16:39:13 -0500

    config client working, with checker property

--- src/main/java/com/bootcamp/personal/passive/savingaccount/SavingAccountApplication.java
+++ src/main/java/com/bootcamp/personal/passive/savingaccount/SavingAccountApplication.java
@@ -1,13 +1,37 @@
 package com.bootcamp.personal.passive.savingaccount;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @SpringBootApplication
-public class SavingAccountApplication {
+public class SavingAccountApplication implements CommandLineRunner {
+
+	private static final Logger logger = Logger.getLogger(SavingAccountApplication.class.toString());
+
+	@Autowired
+	private Environment env;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		//logger.log(Level.INFO, env.getProperty("spring.application.name"));
+		logger.info("Java version: " + env.getProperty("java.version"));
+		logger.info("Application name: " + env.getProperty("spring.application.name"));
+		logger.info("Properties file upload status: " + env.getProperty("my-own-app.properties.status"));
+	}
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SavingAccountApplication.class, args);
+
 	}
 
 }
--- src/main/java/com/bootcamp/personal/passive/savingaccount/SavingAccountController.java
+++ src/main/java/com/bootcamp/personal/passive/savingaccount/SavingAccountController.java
@@ -1,2 +1,64 @@
-package com.bootcamp.personal.passive.savingaccount;public class SavingAccountController {
+package com.bootcamp.personal.passive.savingaccount;
+
+import com.bootcamp.personal.passive.savingaccount.dto.CreateSavingAccountDto;
+import com.bootcamp.personal.passive.savingaccount.dto.UpdateSavingAccountDto;
+import com.bootcamp.personal.passive.savingaccount.entity.SavingAccount;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/saving_account")
+@Tag(name = "Personal Passive Product Saving Account Type", description = "Manage Personal Passive Product saving accounts type")
+@RequiredArgsConstructor
+public class SavingAccountController {
+
+    public final SavingAccountServiceImpl service;
+
+    @GetMapping
+    public Mono<ResponseEntity<Flux<SavingAccount>>> getAll() {
+        return Mono.just(
+                        ResponseEntity.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(service.getAll())
+        );
+    }
+
+    @PostMapping
+    public Mono<ResponseEntity<SavingAccount>> create(@RequestBody CreateSavingAccountDto o) {
+
+        return service.save(o).map( p -> ResponseEntity
+                .created(URI.create("/SavingAccount/".concat(p.getId())))
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(p)
+        );
+    }
+
+    @PutMapping
+    public Mono<ResponseEntity<SavingAccount>> update(@RequestBody UpdateSavingAccountDto o) {
+        return service.update(o)
+                .map(p -> ResponseEntity.created(URI.create("/SavingAccount/"
+                                .concat(p.getId())
+                        ))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(p))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping
+    public Mono<ResponseEntity<SavingAccount>> delete(@RequestBody String id) {
+        return service.delete(id)
+                .map(p -> ResponseEntity.created(URI.create("/SavingAccount/"
+                                .concat(p.getId())
+                        ))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(p))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
 }
