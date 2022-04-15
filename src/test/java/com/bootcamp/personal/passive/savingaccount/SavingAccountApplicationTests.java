a8a076b162d506a5e6284ca222d03c384fe9d7de
samuel14luis <luismendoza.samuel@gmail.com>
2022-04-15 05:42:29 -0500

    Starting spring boot project

---
+++ src/test/java/com/bootcamp/personal/passive/savingaccount/SavingAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.personal.passive.savingaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
